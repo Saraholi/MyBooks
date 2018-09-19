e90c856f7f00f307a6414eccf620303ce38789f6
SARAH APARECIDA DOS SANTOS OLIVEIRA <17259230@CFP127ALUNO.LOCAL>
2018-09-19 10:27:18 -0300

    Conexão Com o Banco de Dados

--- app/src/main/java/br/com/senaijandira/mybooks/CadastroActivity.java
+++ app/src/main/java/br/com/senaijandira/mybooks/CadastroActivity.java
@@ -1,13 +1,124 @@
 package br.com.senaijandira.mybooks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.arch.persistence.room.Room;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+import br.com.senaijandira.mybooks.db.MyBooksDatabase;
+import br.com.senaijandira.mybooks.model.Livro;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    Bitmap livroCapa;
+    ImageView imgLivroCapa;
+    EditText txtTitulo, txtDescricao;
+    AlertDialog alerta;
+
+    private final int COD_REQ_GALERIA = 101;
+
+    private MyBooksDatabase myBooksDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        //Criando a instancia do banco de dados
+        myBooksDb = Room.databaseBuilder(getApplicationContext(),
+                MyBooksDatabase.class, Utils.DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+
+        imgLivroCapa = findViewById(R.id.imgLivroCapa);
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtDescricao = findViewById(R.id.txtDescricao);
     }
+
+    public void abrirGaleria(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        intent.setType("image/*");
+
+        startActivityForResult(Intent.createChooser(intent, "Selecione uma imagem"), COD_REQ_GALERIA);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == COD_REQ_GALERIA && resultCode == Activity.RESULT_OK){
+
+        }try {
+            InputStream input = getContentResolver().openInputStream(data.getData());
+
+            //Coverter  o Bitmap
+            livroCapa = BitmapFactory.decodeStream(input);
+
+            imgLivroCapa.setImageBitmap(livroCapa);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void salvarLivro(View view) {
+
+        byte[] capa = Utils.toByteArray(livroCapa);
+
+        String titulo = txtTitulo.getText().toString();
+
+        String descricao = txtDescricao.getText().toString();
+
+        Livro livro = new Livro(0, capa, titulo, descricao);
+
+       /* //Inserir na variável estática da MainActivity
+        int tamanhoArray = MainActivity.livros.length;
+
+        MainActivity.livros = Arrays.copyOf(MainActivity.livros, tamanhoArray+1);
+
+        MainActivity.livros[tamanhoArray] = livro;
+*/
+
+
+       // Inserir no Banco de Dados.
+        myBooksDb.daoLivro().inserir(livro);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Salvar");
+        //define a mensagem
+        builder.setMessage("Livro salvo com sucesso!");
+        //define um botão como positivo
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+
+            }
+        });
+        //define um botão como negativo.
+        builder.setNegativeButton("Negativo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+
+            }
+        });
+        //cria o AlertDialog
+        alerta = builder.create();
+        //Exibe
+        alerta.show();
+    }
+
 }
--- app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
+++ app/src/main/java/br/com/senaijandira/mybooks/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.senaijandira.mybooks;
 
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import br.com.senaijandira.mybooks.db.MyBooksDatabase;
 import br.com.senaijandira.mybooks.model.Livro;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout  listaLivros;
 
     public static Livro[] livros;
+
+    // Variavel de acesso ao Banco
+    private MyBooksDatabase myBooksDb;
 
 
 
@@ -25,12 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //Criando Instancias do Banco de Dados.
+        myBooksDb = Room.databaseBuilder(getApplicationContext(),
+                MyBooksDatabase.class,Utils.DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+
+
         listaLivros = findViewById(R.id.listaLivros);
 
         //Criar um Cadastros Fake
 
-        livros = new Livro[]{
-           new Livro(1,Utils.toByteArray(getResources(),R.drawable.pequeno_principe),
+    livros = new Livro[]{
+ /*          new Livro(1,Utils.toByteArray(getResources(),R.drawable.pequeno_principe),
                    "O pequeno principe",getString(R.string.pequeno_principe)),
 
         new Livro(2,Utils.toByteArray(getResources(),R.drawable.cinquenta_tons_cinza),
@@ -41,37 +54,63 @@ public class MainActivity extends AppCompatActivity {
                 "Kotlin com Android",getString(R.string.pequeno_principe)),
 
 
+*/
 
         };
 
     }
+
+
+
+
+
+
         @Override
         protected void onResume(){
-
         super.onResume();
 
-        listaLivros.removeAllViews();
+        // Aqui faz um SELECT no Banco.
+        livros = myBooksDb.daoLivro().selecionarTodos();
 
+        listaLivros.removeAllViews();
 
         for (Livro l : livros){
             criarLivros(l,listaLivros);
         }
     }
 
+    // Aqui faz o DELETE no Banco
+    private void deletaLivro(Livro livro, View v){
+        //Remover do Banco de dados
+        myBooksDb.daoLivro().deletar(livro);
+
+        // Remover da Tela
+        listaLivros.removeView(v);
+
+    }
 
 
 
+    public void criarLivros(final Livro livro, ViewGroup root) {
 
-
-    public void criarLivros(Livro livro, ViewGroup root) {
-
-            View v = LayoutInflater.from(this)
+            final View v = LayoutInflater.from(this)
             .inflate(R.layout.livro_layout, root, false);
 
 
         ImageView imgLivroCapa = v.findViewById(R.id.imglivroCapa);
         TextView txtLivroTitulo = v.findViewById(R.id.txtLivroTitulo);
         TextView txtLivroDescricao = v.findViewById(R.id.txtLivroDescricao);
+
+        ImageView imgDeleteLivro = v.findViewById(R.id.imgDeleteLivro);
+
+        imgDeleteLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deletaLivro(livro,v);
+
+            }
+        });
 
 
         imgLivroCapa.setImageBitmap(Utils.toBitmap(livro.getCapa()));
